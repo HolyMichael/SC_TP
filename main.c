6ba7992cacdf7ced2af87faef5d4bcee91f6686c
HolyMichael <555migalves555@gmail.com>
2018-12-22 01:10:31 +0000

    falta prioridades

--- main.c
+++ main.c
@@ -32,12 +32,17 @@ void makeEvent(int tipo,int tempo,int posto);
 void inserirEventoInicio( Evento*nv);
 void inserirEventoFim( Evento *nv);
 void inserirEventoOrdem(Evento*nv);
-void imprimeLista();
 void processarEventoChegada();
-void imprimeFilaVendedores();
 void processarEventoSaidaVendedores();
+void processarEventoSaidaPagamento();
+void processarEventoLevantamentoPagamento();
 void makeEventVendedoresSaida(int posto);
 void makeEventPagamentoSaida(int posto);
+void makeEventLevantamentoSaida(int posto);
+void imprimeLista();
+void imprimeFilaVendedores();
+void imprimeFilaPagamento();
+void imprimeFilaLevantamento();
 
 
 //Variaveis Globais
@@ -46,11 +51,12 @@ void makeEventPagamentoSaida(int posto);
  #define minClients 300
  #define maxClients 320
 int cli;
- int probPrioritario = 5;
-
+int probPrioritario = 5;
+int retornados=0;
+int saidavendedores=0;
 //percentagem de clients por hora (Entrada de Clientes)
-int percentagem1 = 99; //10-13h
-int percentagem2 = 1; //13-16h
+int percentagem1 = 10; //10-13h
+int percentagem2 = 30; //13-16h
 int percentagem3 = 15; //16-19h
 //  percentagem 4 é o restante para 100
 
@@ -63,49 +69,88 @@ struct Cliente *vendedoresFila[maxClients][10]= {NULL};
 
 //Postos Pagamento 
 struct Cliente *pagamentoPostos[4]={NULL};
-struct Cliente *pagamentoFila[maxClients];
+struct Cliente *pagamentoFila[maxClients] = {NULL};
+
+//Postos Levantamento 
+struct Cliente *levantamentoPostos[2]={NULL};
+struct Cliente *levantamentoFila[maxClients] = {NULL};
 
 int main() {
 	int tipoEvento;
 	int eventosprocessados=0;
+	int eventoschegada=0;
+	int eventospagamento=0;
+	int eventosvendedor=0;
+	int eventoslevantamento=0;
 	srand(time(NULL));
 	setup();
+	saidavendedores=cli;
     imprimeLista();
+    printf("\nNUM_CLIENTES: %d", cli);
 	printf("\n\n\n");
 	while(evento!=NULL){
 		tipoEvento=evento->tipo;
 		relogio=evento->tempoOcorrencia;
 		eventosprocessados++;
+		printf("\n------ UM NOVO EVENTO ------\n");
+		switch(tipoEvento){
+			case 0:
+				printf("EVENTO_TIPO: 0 CHEGADA CLIENTE\n");
+				break;
+			case 1:
+				printf("EVENTO_TIPO: 1 SAIDA VENDEDORES\n");
+				break;
+			case 2:
+				printf("EVENTO_TIPO: 2 SAIDA PAGAMENTO\n");
+				break;
+			case 3:
+				printf("EVENTO_TIPO: 3 SAIDA LEVANTAMENTO\n");
+				break;
+		}
+		printf("NUM_EVENTOS: %d\n", eventosprocessados);
+		printf("CLOCK_ATUAL: %d\n",relogio);
 		switch(tipoEvento){
 			//Chegada Cliente
-			case 0: 
+			case 0:
 				processarEventoChegada();
-				//imprimeFilaVendedores(); //flush it to file
+				imprimeFilaVendedores(); //flush it to file
+				eventoschegada++;
 				break; 
 			//Fim Zona Vendedores
-			case 1: 
+			case 1:
 				processarEventoSaidaVendedores();
-				imprimeFilePagamento(); //flush it to file
+				imprimeFilaVendedores();
+				imprimeFilaPagamento();
+				eventosvendedor++;
 				break;
 			//Fim Zona Pagamento
 			case 2:
-				;
+				processarEventoSaidaPagamento();
+				imprimeFilaPagamento();
+				imprimeFilaLevantamento();
+				eventospagamento++;
 				break;
 			//Zona de Levantamento
 			case 3:
-				;
+				processarEventoSaidaLevantamento();
+				imprimeFilaLevantamento();
+				eventoslevantamento++;
 				break;
 	   }
-		printf("\nEVENTO: %d\n", tipoEvento);
-		printf("\nNUM_EVENTOS: %d\n", eventosprocessados);
-		printf("\nNUM_CLIENTES: %d\n", cli);
-		//imprimeFilaVendedores();
 	 evento=evento->nseg;
 	}
-
-	printf("\nCHEGAMOS AO FIM BOIISS  %d\n", eventosprocessados);
-	imprimeLista();
+	printf("-------   FIM   -------");
 	imprimeFilaVendedores();
+	imprimeFilaPagamento();
+	imprimeFilaLevantamento();
+	printf("\n\n %d clientes, e %d eventos processados\n", cli, eventosprocessados);
+	printf("\n Eventos de chegada            : %d",eventoschegada);
+	printf("\n Eventos de saida vendedores   : %d",eventosvendedor);
+	printf("\n Eventos de saida pagamento    : %d",eventospagamento);
+	printf("\n Eventos de saida levantamento : %d\n",eventoslevantamento);
+	printf("\n RETORNADOS                    : %d", retornados);
+	printf("\n SAIDAS NOS VENDEDORES         : %d", saidavendedores);
+	imprimeLista();
 }
 
 void makeEventPagamentoSaida(int posto){
@@ -125,9 +170,56 @@ void makeEventPagamentoSaida(int posto){
 	tempo +=relogio;
     
     makeEvent(2,tempo,posto);
-	printf("\n\n %d clock:%d\n",tempo,relogio);	
+	printf("\n Novo Evento gerado:\n ETIPO:2\n TEMPO:%d\n",tempo);	
 	//imprimeLista();
 
+}
+
+void processarEventoSaidaLevantamento(){
+	int i;
+	int posto=evento->posto;
+	int prob= rand() % 101;
+  	
+    // Clientes que regressam ao pagamento
+	if(prob<6){
+		retornados++;
+		int flag=0;
+		for(i=0;i<4;i++){
+		//Ocupar posto, se existir
+			if(pagamentoPostos[i]==NULL){
+				makeEventPagamentoSaida(i);
+				pagamentoPostos[i] = levantamentoPostos[posto];
+				flag=1;
+				break;
+			}
+    	}
+    	//caso não exista posto livre ocupar fila
+    	int i;
+    	for(i=0;i<maxClients;i++){
+    		if(pagamentoFila[i]==NULL && flag == 0){
+    			pagamentoFila[i] = levantamentoPostos[posto];
+    			break;
+    		}
+    	}
+	}
+
+	// Limpeza de filas e postos. Quem nao compra vai embora.
+	if(levantamentoFila[0]==NULL){
+		levantamentoPostos[posto]=NULL;
+		return;
+	}
+	
+	//mover o cliente da fila para o atendimento e fazer o seu evento
+	levantamentoPostos[posto]=levantamentoFila[0];
+	makeEventLevantamentoSaida(posto);
+	
+    for(i=1; i<maxClients/10; i++){
+		levantamentoFila[i-1]=levantamentoFila[i];
+		if(levantamentoFila[i]==NULL){
+			levantamentoFila[i-1]=NULL;
+			break;
+		}	
+	}
 }
 
 void processarEventoSaidaVendedores(){
@@ -137,21 +229,30 @@ void processarEventoSaidaVendedores(){
   	
     // Clientes que compram
 	if(prob<81){
-		for(i=0;i<10;i++){
+		saidavendedores--;
+		int flag=0;
+		for(i=0;i<4;i++){
 		//Ocupar posto, se existir
 			if(pagamentoPostos[i]==NULL){
 				makeEventPagamentoSaida(i);
 				pagamentoPostos[i] = vendedoresPostos[posto];
+				flag=1;
 				break;
 			}
+    	}
+    	//caso não exista posto livre ocupar fila
+    	int i;
+    	for(i=0;i<maxClients;i++){
+    		if(pagamentoFila[i]==NULL && flag == 0){
+    			pagamentoFila[i] = vendedoresPostos[posto];
+    			break;
+    		}
     	}
 	}
 
 	// Limpeza de filas e postos. Quem nao compra vai embora.
-    printf("%p",vendedoresFila[0][posto]);
 	if(vendedoresFila[0][posto]==NULL){
 		vendedoresPostos[posto]=NULL;
-		printf("VAMO RETORNAR GALERA");
 		return;
 	}
 	vendedoresPostos[posto]=vendedoresFila[0][posto];
@@ -159,12 +260,11 @@ void processarEventoSaidaVendedores(){
     for(i=1; i< maxClients/10; i++){
 		vendedoresFila[i-1][posto]=vendedoresFila[i][posto];
 		if(vendedoresFila[i][posto]==NULL){
-			printf("\ncaso especail\n");
 			vendedoresFila[i-1][posto]=NULL;
 			break;
 		}	
 	}
-
+	//criamos o evento do cliente agora a ser atendido no posto livre.
 	makeEventVendedoresSaida(posto);
 }
 
@@ -187,12 +287,29 @@ void makeEventVendedoresSaida(int posto){
 	tempo +=relogio;
     
     makeEvent(1,tempo,posto);
-	printf("\n\n %d clock:%d\n",tempo,relogio);	
-	//imprimeLista();
+	printf("\n Novo Evento gerado:\n ETIPO:1\n TEMPO:%d\n",tempo);
 
 }
 
+void makeEventLevantamentoSaida(int posto){
+	int tempe,tempo; 
+	tempe= rand() % 101;
 
+	if( tempe < 31)
+		tempo= rand () % 301;
+	if(tempe > 30 && tempe < 71)
+		tempo= rand() % 301 +300;
+	if(tempe > 70 && tempe < 96)
+		tempo= rand() % 301+ 600;
+	if(tempe > 95)
+		tempo = rand () % 301 + 900;
+
+	tempo +=relogio;
+    
+    makeEvent(3,tempo,posto);
+	printf("\n Novo Evento gerado:\n ETIPO:3\n TEMPO:%d\n",tempo);
+
+}
 
 void processarEventoChegada(){
 	int i=0,j=0;
@@ -254,8 +371,55 @@ void processarEventoChegada(){
 			break;
 		}
 	}
+}
+
+void processarEventoSaidaPagamento(){
+	int i;
+	int posto=evento->posto;
+	int prob= rand() % 101;
+	
+	// Clientes que levantam
+	if(prob<61){
+		int flag=0;
+		for(i=0;i<2;i++){
+		//Ocupar posto, se existir
+			if(levantamentoPostos[i]==NULL){
+				makeEventLevantamentoSaida(i); //LEVANTAMENTO!
+				levantamentoPostos[i] = pagamentoPostos[posto];
+				flag=1;
+				break;
+			}
+    	}
+    	//caso não exista posto livre ocupar fila
+    	int i;
+    	for(i=0;i<maxClients;i++){
+    		if(levantamentoFila[i]==NULL && flag == 0){
+    			levantamentoFila[i] = pagamentoPostos[posto];
+    			break;
+    		}
+    	}
+	}
 	
 	
+	// Limpeza de filas e postos. Quem nao compra vai embora.
+	//Fila de pagamento vazia
+	if(pagamentoFila[0]==NULL){
+		pagamentoPostos[posto]=NULL;
+		return;
+	}
+	//mover o primeiro cliente na fila para o posto
+	pagamentoPostos[posto]=pagamentoFila[0];
+	//criamos o evento do cliente agora a ser atendido no posto livre.
+	makeEventPagamentoSaida(posto);
+	
+	//mover toda a fila 1 para a frente
+    for(i=1; i< maxClients; i++){
+		pagamentoFila[i-1]= pagamentoFila[i];
+		if(pagamentoFila[i]==NULL){
+			pagamentoFila[i-1]=NULL; //fazemos null a ultima posição pois trata-se de um cliente duplicado
+			break;
+		}	
+	}
 }
 
 void setup(){
@@ -384,7 +548,7 @@ void imprimeLista(){
 
 
 	Evento *aux=evento;
-
+	printf("\nLista de eventos:");
 	if(aux==NULL)
 		printf("Vazio");
 		while(aux!=NULL){
@@ -394,8 +558,23 @@ void imprimeLista(){
 	printf("\n");
 }
 
-void imprimeFilePagamento(){
+void imprimeFilaLevantamento(){
 	int i;
+	printf("\n\nFila de Levantamento\n");
+	printf("Postos: %i | %i\n", (levantamentoPostos[0]!=NULL),(levantamentoPostos[1]!=NULL));
+	printf("Filas :");
+	for(i = 0; i< maxClients ;i++){
+		if(levantamentoFila[i]!=NULL)
+			printf("| 1");
+		else break;
+			
+	}
+	printf("\n");
+}
+
+void imprimeFilaPagamento(){
+	int i;
+	printf("\n\nFila de pagamento\n");
 	printf("Postos: %i | %i | %i | %i\n", (pagamentoPostos[0]!=NULL),(pagamentoPostos[1]!=NULL),
 		(pagamentoPostos[2]!=NULL),(pagamentoPostos[3]!=NULL));
 	printf("Filas :");
@@ -405,10 +584,12 @@ void imprimeFilePagamento(){
 		else break;
 			
 	}
+	printf("\n");
 }
 void imprimeFilaVendedores(){
 	int i,j;
 	int finish=0;
+	printf("\n\nFila de vendedores\n");
 	printf("Postos|");
 	for(i=0;i<10;i++){
 		if(vendedoresPostos[i]==NULL)
@@ -430,6 +611,7 @@ void imprimeFilaVendedores(){
 	printf("\n      |");
 	else return;
 	}	
+	printf("\n");
 }
 	
 	
